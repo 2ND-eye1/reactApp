612e13b0802d4efe68c33204a53d1a758a6a16d2
Ndeye Fatou Diop <n.fatoudiop05@gmail.com>
2025-06-29 13:19:55 +0100

    commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // we want to notify when smtg chagnes in the listgroup componenet
 import { NavBar } from "./components/NavBar";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Body></Body>
     </div>
   );
 }
