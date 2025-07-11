380f8f755612358c32eafaf65a32bd8d6730ec68
Ndeye Fatou Diop <n.fatoudiop05@gmail.com>
2025-07-11 16:26:52 +0100

    commint changes to StarRating

--- src/components/Rating/StarRating.tsx
+++ src/components/Rating/StarRating.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 function StarRating() {
   // now we have to change the rating based on the star is clicked using the useState hook
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  // this is so that the user can hover on the starts and see what the rating would be.
 
   return (
     <div className="rating">
@@ -15,23 +17,23 @@ function StarRating() {
         const ratingValue = i + 1;
 
         return (
-          <>
-            <label key={ratingValue} className={style.hearts}>
-              <input
-                type="radio"
-                name="rating"
-                value={ratingValue}
-                onClick={() => setRating(ratingValue)}
-              />
-              {/* // in order to add
+          <label key={ratingValue} className={style.hearts}>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            {/* // in order to add
             something to each button we need to add a label to wrap the smtg
             with the input radio */}
-              <FaHeart
-                size={20}
-                color={ratingValue <= rating ? "red" : "gray"}
-              />
-            </label>
-          </>
+            <FaHeart
+              size={20}
+              color={ratingValue <= (hover || rating) ? "red" : "#e4e5e9"}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(0)}
+            />
+          </label>
         );
       })}
     </div>
